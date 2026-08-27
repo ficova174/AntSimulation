3e59b7ff4ea8dd2a2f2145b799e4dacae2748c1e
Ficova <axel.letrong@gmail.com>
2026-08-27 11:11:01 +0200

    camera and brush are independent

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -1,9 +1,16 @@
 extends Camera2D
 
+@export var move_speed: float = 500.0
+@export var zoom_speed: float = 0.1
 
-@export var speed: float = 500.0
 
+func _input(event: InputEvent) -> void:
+	if not event.is_action_pressed("brush_size_up") and not event.is_action_pressed("brush_size_down"):
+		if event.is_action_pressed("zoom_in") and zoom != Vector2.ZERO:
+			zoom += Vector2.ONE * zoom_speed
+		elif event.is_action_pressed("zoom_out") and zoom != Vector2.ZERO:
+			zoom -= Vector2.ONE * zoom_speed
 
 func _process(delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed * delta
+	position += direction * move_speed * delta
