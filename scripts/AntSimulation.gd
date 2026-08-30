1712b6bc7056b97fcdeb3f942af8ab0cdf518faa
Ficova <axel.letrong@gmail.com>
2026-08-30 18:34:14 +0200

    ants that follow and put pheromones

--- scripts/AntSimulation.gd
+++ scripts/AntSimulation.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-const NUM_ANTS: int = 100
-const HEIGHT: int = 512
-const WIDTH: int = 512
+const NUM_ANTS: int = 20
+const HEIGHT: int = 2048
+const WIDTH: int = 2048
 
 var radius: float = 30.0
 var radius_delta: float = 1.0
@@ -11,6 +11,9 @@ var circle_color: Color = Color.BLUE
 var filled: bool = false
 
 var rd: RenderingDevice
+
+var pheromones_shader_file: Resource = preload("res://shaders/pheromones.glsl")
+var ants_shader_file: Resource = preload("res://shaders/ants.glsl")
 
 var pheromones_shader: RID
 var ants_shader: RID
@@ -33,8 +36,11 @@ var uniform_set_b_to_a: RID
 var uniform_set_ants_a: RID
 var uniform_set_ants_b: RID
 
-var display_tex: Texture2DRD
+var display_pheromones_tex: Texture2DRD
 var step_toggle: bool = false
+
+var ant_sprites: Array[Sprite2D] = []
+var ant_texture: Resource = preload("res://assets/ant.png")
 
 
 func _ready() -> void:
@@ -43,10 +49,19 @@ func _ready() -> void:
 	_init_ants()
 
 func _init_pheromones() -> void:
-	var pheromones_shader_file: Resource = preload("res://shaders/pheromones.glsl")
 	pheromones_shader = rd.shader_create_from_spirv(pheromones_shader_file.get_spirv())
 	pheromones_pipeline = rd.compute_pipeline_create(pheromones_shader)
 
+	var tf: RDTextureFormat = _create_texture_format()
+	pheromones_tex_a = rd.texture_create(tf, RDTextureView.new())
+	pheromones_tex_b = rd.texture_create(tf, RDTextureView.new())
+
+	_init_pheromones_map()
+
+	uniform_set_a_to_b = _create_set_pheromones(pheromones_tex_a, pheromones_tex_b)
+	uniform_set_b_to_a = _create_set_pheromones(pheromones_tex_b, pheromones_tex_a)
+
+func _create_texture_format() -> RDTextureFormat:
 	var tf: RDTextureFormat = RDTextureFormat.new()
 	# Format must match the data format specified in the shader
 	tf.format = RenderingDevice.DATA_FORMAT_R8G8B8A8_UNORM
@@ -59,17 +74,14 @@ func _init_pheromones() -> void:
 	tf.height = HEIGHT
 	tf.width = WIDTH
 
-	pheromones_tex_a = rd.texture_create(tf, RDTextureView.new())
-	pheromones_tex_b = rd.texture_create(tf, RDTextureView.new())
+	return tf
 
-	display_tex = Texture2DRD.new()
-	display_tex.texture_rd_rid = pheromones_tex_a
-	$PheromonesMap.texture = display_tex
+func _init_pheromones_map() -> void:
+	display_pheromones_tex = Texture2DRD.new()
+	display_pheromones_tex.texture_rd_rid = pheromones_tex_a
+	$PheromonesMap.texture = display_pheromones_tex
 	rd.texture_clear(pheromones_tex_a, Color(0.0, 0.0, 1.0, 1.0), 0, 1, 0, 1)
 	rd.texture_clear(pheromones_tex_b, Color(0.0, 0.0, 1.0, 1.0), 0, 1, 0, 1)
-
-	uniform_set_a_to_b = _create_set_pheromones(pheromones_tex_a, pheromones_tex_b)
-	uniform_set_b_to_a = _create_set_pheromones(pheromones_tex_b, pheromones_tex_a)
 
 func _create_set_pheromones(read_tex: RID, write_tex: RID) -> RID:
 	var u_read: RDUniform = RDUniform.new()
@@ -85,10 +97,15 @@ func _create_set_pheromones(read_tex: RID, write_tex: RID) -> RID:
 	return rd.uniform_set_create([u_read, u_write], pheromones_shader, shader_set)
 
 func _init_ants() -> void:
-	var ants_shader_file: Resource = preload("res://shaders/ants.glsl")
 	ants_shader = rd.shader_create_from_spirv(ants_shader_file.get_spirv())
 	ants_pipeline = rd.compute_pipeline_create(ants_shader)
 
+	ants_buffer = _create_ants_buffer()
+
+	uniform_set_ants_a = _create_set_ants(pheromones_tex_a)
+	uniform_set_ants_b = _create_set_ants(pheromones_tex_b)
+
+func _create_ants_buffer() -> RID:
 	var ants_data: PackedByteArray = PackedByteArray()
 	var ant_data_size: int = 16
 	ants_data.resize(NUM_ANTS * ant_data_size)
@@ -99,7 +116,9 @@ func _init_ants() -> void:
 		var offset: int = i * ant_data_size
 
 		var angle: float = randf() * TAU
-		var pos: Vector2 = center + Vector2(cos(angle), sin(angle)) * (randf() * 20.0)
+		var pos: Vector2 = center + Vector2(cos(angle), sin(angle)) * (randf() * 200.0)
+
+		_create_ant_sprite(pos, angle)
 
 		# float takes 4 bytes and we need padding bc a struct offset
 		# must be a multiple of its biggest element for aligment purposes
@@ -108,10 +127,16 @@ func _init_ants() -> void:
 		ants_data.encode_float(offset + 8, angle)
 		ants_data.encode_float(offset + 12, 0.0)
 
-	ants_buffer = rd.storage_buffer_create(ants_data.size(), ants_data)
+	return rd.storage_buffer_create(ants_data.size(), ants_data)
 
-	uniform_set_ants_a = _create_set_ants(pheromones_tex_a)
-	uniform_set_ants_b = _create_set_ants(pheromones_tex_b)
+func _create_ant_sprite(pos: Vector2, angle: float) -> void:
+		var ant_sprite: Sprite2D = Sprite2D.new()
+		ant_sprite.texture = ant_texture
+		ant_sprite.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+		ant_sprite.position = pos
+		ant_sprite.rotation = angle
+		$AntsContainer.add_child(ant_sprite)
+		ant_sprites.append(ant_sprite)
 
 func _create_set_ants(current_tex: RID) -> RID:
 	# Binding 0: ants uniform buffer (reading/writing)
@@ -185,8 +210,18 @@ func _compute_pheromones(delta: float) -> void:
 	rd.compute_list_end()
 
 func _process(_delta: float) -> void:
-	var latest_output: RID = pheromones_tex_b if step_toggle else pheromones_tex_a
-	display_tex.texture_rd_rid = latest_output
+	var raw_data: PackedByteArray = rd.buffer_get_data(ants_buffer)
+	for i in range(NUM_ANTS):
+		var offset: int = i * 16
+		var pos_x: float = raw_data.decode_float(offset + 0)
+		var pos_y: float = raw_data.decode_float(offset + 4)
+		var angle: float = raw_data.decode_float(offset + 8)
+
+		ant_sprites[i].position = Vector2(pos_x, pos_y)
+		ant_sprites[i].rotation = angle
+
+	var latest_pheromones_tex: RID = pheromones_tex_b if step_toggle else pheromones_tex_a
+	display_pheromones_tex.texture_rd_rid = latest_pheromones_tex
 
 	queue_redraw()
 
--- shaders/ants.glsl
+++ shaders/ants.glsl
@@ -20,5 +20,40 @@ layout(rgba8, set = 0, binding = 1) uniform restrict image2D pheromones;
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 
 void main() {
-    return;
+    uint id = gl_GlobalInvocationID.x;
+    float speed = 100.0;
+    float rotation_speed = 10.0;
+
+    vec2 left_antenna_local = vec2(15.0, -6.0);
+    vec2 right_antenna_local = vec2(15.0, 6.0);
+    vec2 pheromones_gland_local = vec2(-12.0, 0.0);
+    mat2 rotation_angle = mat2(
+        vec2(cos(ants[id].angle), sin(ants[id].angle)),
+        vec2(-sin(ants[id].angle), cos(ants[id].angle))
+    );
+    vec2 left_antenna_rotated = rotation_angle * left_antenna_local;
+    vec2 right_antenna_rotated = rotation_angle * right_antenna_local;
+    vec2 pheromones_gland_rotated = rotation_angle * pheromones_gland_local;
+
+    vec4 left_antenna_pheromones = imageLoad(pheromones, ivec2(ants[id].position + left_antenna_rotated));
+    vec4 right_antenna_pheromones = imageLoad(pheromones, ivec2(ants[id].position + right_antenna_rotated));
+    imageStore(pheromones, ivec2(ants[id].position + pheromones_gland_rotated), vec4(1.0, 0.0, 0.0, 1.0));
+
+    bool is_pheromones = left_antenna_pheromones.r >= 0.01 || right_antenna_pheromones.r >= 0.01;
+    bool turn_left = left_antenna_pheromones.r > right_antenna_pheromones.r;
+    bool turn_right = left_antenna_pheromones.r < right_antenna_pheromones.r;
+
+    if (is_pheromones) {
+        if (turn_left) {
+            // Pay attention to godot axis and rotation direction
+            ants[id].angle -= rotation_speed * params.time_elapsed;
+        } else if (turn_right) {
+            ants[id].angle += rotation_speed * params.time_elapsed;
+        }
+    } // else if (left_antenna_pheromones.r < 0.01 && right_antenna_pheromones < 0.01) {
+    //     random_direction;
+    // }
+
+    vec2 forward = rotation_angle * vec2(1.0, 0.0);
+    ants[id].position += forward * speed * params.time_elapsed;
 }
